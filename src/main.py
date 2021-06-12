b989c5e517ead77f40a9ec3ba15889fb720fdd0e
Alex Emelyanov <alex84_84_84@mail.ru>
2021-06-12 19:33:05 +0300

    test arch_path

--- src/main.py
+++ src/main.py
@@ -56,6 +56,7 @@ def import_ovis(api: sly.Api, task_id, context, state, app_logger):
     api.file.download(TEAM_ID, cur_files_path, archive_path)
 
     if tarfile.is_tarfile(archive_path):
+        logger.warn('archive_path tar: {}'.format(archive_path))
         with tarfile.open(archive_path) as archive:
             archive.extractall(extract_dir)
     else:
@@ -87,12 +88,16 @@ def import_ovis(api: sly.Api, task_id, context, state, app_logger):
             logger.warn('There is no archive {} in the input data, but it must be'.format(arch_name))
             continue
 
-        shutil.unpack_archive(arch_path, input_dir)
-        if zipfile.is_zipfile(archive_path):
-            with zipfile.ZipFile(archive_path, 'r') as archive:
+        #shutil.unpack_archive(arch_path, input_dir)
+        logger.warn('{}'.format(os.listdir(input_dir)))
+        logger.warn('archive_path: {}'.format(archive_path))
+        logger.warn('arch_path: {}'.format(arch_path))
+        if zipfile.is_zipfile(arch_path):
+            logger.warn('is it work: {}'.format(archive_path))
+            with zipfile.ZipFile(arch_path, 'r') as archive:
                 archive.extractall(input_dir)
         else:
-            raise Exception("No such file".format(INPUT_FILE))
+            raise Exception("No such file".format(archive_path))
 
         imgs_dir_path = os.path.join(input_dir, sly.fs.get_file_name(arch_name))
         ann_json = sly.json.load_json_file(ann_path)
@@ -157,10 +162,14 @@ def import_ovis(api: sly.Api, task_id, context, state, app_logger):
             frames = []
             for idx in range(len(images)):
                 figures = []
-                for curr_ann in curr_anns:
+                for fig_id, curr_ann in enumerate(curr_anns):
                     ovis_geom = curr_ann[2][idx]
                     if ovis_geom:
                         mask = decode(ovis_geom).astype(bool)
+                        if img_size[1] % 2 == 1:
+                            mask[mask.shape[0] - 1, :] = False
+                        if img_size[0] % 2 == 1:
+                            mask[:, mask.shape[1] - 1] = False
                         geom = sly.Bitmap(mask)
                         figure = sly.VideoFigure(video_objects[curr_ann[1]], geom, idx)
                         figures.append(figure)
